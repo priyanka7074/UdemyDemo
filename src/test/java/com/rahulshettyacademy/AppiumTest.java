a819cb64c0736fa5b4ff3fd5b0238a7fe39df54b
priyanka7074 <priyanka.katarey@gmail.com>
2020-04-30 11:41:01 +0530

    my first commit

---
+++ src/test/java/com/rahulshettyacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package com.rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeAppAndroid(){
+		System.out.println("nativeAppAndroid");
+		
+	}
+	
+	@Test
+	public void iosApps(){
+		System.out.println("iosApps");
+		
+	}
+
+}
